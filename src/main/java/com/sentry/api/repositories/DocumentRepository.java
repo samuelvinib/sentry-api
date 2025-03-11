f40ab4b6abdd3969b1b3ef9fb2f8f576607b1d3a
Samuel Vinícius <samuelvinib@gmail.com>
2025-03-11 18:25:22 -0300

    feat: Criado repository para os models Document, Token e User.

---
+++ src/main/java/com/sentry/api/repositories/DocumentRepository.java
@@ -0,0 +1,9 @@
+package com.sentry.api.repositories;
+
+import com.sentry.api.models.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+}
---
+++ src/main/java/com/sentry/api/repositories/TokenRepository.java
@@ -0,0 +1,9 @@
+package com.sentry.api.repositories;
+
+import com.sentry.api.models.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token,Long> {
+}
