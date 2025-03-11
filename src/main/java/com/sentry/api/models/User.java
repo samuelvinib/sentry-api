7527a91124e1177704ab096ab9443f286c1c4a85
Samuel Vinícius <samuelvinib@gmail.com>
2025-03-11 00:55:52 -0300

    feat: Criado modelo User
    Este commit adiciona o modelo User para criação de Conta no sistema.
    
    Campos:
    - email
    - password

---
+++ src/main/java/com/sentry/api/models/User.java
@@ -0,0 +1,24 @@
+package com.sentry.api.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
