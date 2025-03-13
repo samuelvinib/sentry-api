cd57331ae2d6643f449faa411316704fcd612357
Samuel Vinícius <samuelvinib@gmail.com>
2025-03-13 00:26:16 -0300

    feat: ErrorResponse adicionado ao projeto para respostas mais assertivas no body da requisição.

---
+++ src/main/java/com/sentry/api/config/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.sentry.api.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ErrorResponse {
+    private String message;
+    private String details;
+
+    public ErrorResponse(String message, String details) {
+        this.message = message;
+        this.details = details;
+    }
+
+}
